6aa877e3c4c6fa58a31eb543738bb7b0f2c89962
Chase Thompson <Nuclearpheasant@hotmail.com>
2017-09-05 09:18:48 -0500

    Update Madlibs
    Fix errors in the Madlibs program, change the story, add the int into the program.

--- src/madlibs/Main.java
+++ src/madlibs/Main.java
@@ -8,29 +8,42 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "There once was a " + 
-		input("Please input an adjective", "red") + " " +
+		JOptionPane.showMessageDialog(null, "The " + 
+		input("Please input color", "red") +
+		" Dragon is the  " +
+		input("Please input a word ending in -est", "tallest") +
+		" Dragon of all. It has " +
+		inputint("Please input a number", "55") + 
+		input("Please input a body part plural", "arms") +
+		", and a " + 
 		input("Please input an animal", "horse") +
-		" who lived in a " +
-		input("Please input a noun", "house") + ". " +
-		"One day he started walking around some " +
-		input("Please input a plural noun", "schools") + " " +
-		"when all of a sudden " +
-		input("Please input a celebrity", "John Cena") +
-		" jumped out and hurt his " + 
-		input("Please input a body part", "arms") + 
-		" making him the " +
-		input("Please input a word ending in -est", "tallest") + 
-		" thing in existance."); 
+		" shaped like a " + 
+		input("Please input a noun", "house") + 
+		". It loves to eat " +
+		input("Please input a plural noun", "schools") + 
+		", although it will feast on nearly anything."); 
 	}
 	
 	/**
 	 * Creates a dialog box that asks for input of a specific thing
-	 * @param message The item that is needed for input
-	 * @param defaultAnswer The default answer being displayed in the input box
+	 * @param message Describe the item needed for input
+	 * @param defaultAnswer The default answer being displayed
 	 * @return Returns the input of the dialog box and adds it to the story
 	 */
 	private static String input(String message, String defaultAnswer) {
 		return JOptionPane.showInputDialog(message, defaultAnswer);
+	}
+	
+	/**
+	 * Creates a dialog box that asks for a number
+	 * @param message Describes the item needed for input
+	 * @param defaultAnswer The default answer being displayed
+	 * @return Returns the input of the dialog box and adds it to the story
+	 */
+	private static int inputint(String message, String defaultAnswer) {
+		int a = Integer.parseInt(JOptionPane.showInputDialog(message, defaultAnswer));
+		int b = (int)(Math.random() * 100);
+		int c = a - b;
+		return c;
 	}
 }
