a0b793549046d7723c9f9e9190fc5a0e37c3bbd2
Chase Thompson <Nuclearpheasant@hotmail.com>
2017-09-07 09:29:04 -0500

    Start RandomGuess
    Start RandomGuess, displays everything except difference in number.

---
+++ src/gamezone/RandomGuessMatch.java
@@ -0,0 +1,19 @@
+package gamezone;
+
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+public class RandomGuessMatch {
+	public static void main(String[] args) {
+		Random rand = new Random();
+		int randomNum = (int)rand.nextInt((5 - 1) + 1) + 1;
+		int guess = Integer.parseInt(JOptionPane.showInputDialog("Guess a number between 1-5"));
+		if (guess == randomNum) {
+			JOptionPane.showMessageDialog(null, "You guessed right! \nThe number was " + randomNum);
+		} else {
+			JOptionPane.showMessageDialog(null, "You guessed wrong. \nThe number was " + randomNum +
+												"\nThe difference: " + (randomNum - guess));
+		}
+	}
+}
